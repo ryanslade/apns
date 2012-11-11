7843d0dd089745406402de58d03765c3f55c12c3
Ryan Slade <ryanslade@gmail.com>
2012-11-11 10:48:20 +0000

    Added friendly error printing for APNS errors

--- error.go
+++ error.go
@@ -11,8 +11,32 @@ type apnsError struct {
 	identifier uint32
 }
 
+var (
+	FriendlyMessages []string = []string{
+		"No Errors",
+		"Processing Error",
+		"Missing Device Token",
+		"Missing Topic",
+		"Missing Payload",
+		"Invalid Token Size",
+		"Invalid Topic Size",
+		"Invalid Payload Size",
+		"Invalid Token",
+	}
+)
+
+const (
+	UnknownErrorMessage = "Unknown error"
+)
+
 func (e apnsError) Error() string {
-	return fmt.Sprintf("APNS Error: Command %v, Status %v, Identifier %v", e.command, e.status, e.identifier)
+	formatString := fmt.Sprint("APNS Error: %v for payload ", e.identifier)
+
+	if e.command != 8 || e.status < 0 || e.status > uint8(len(FriendlyMessages)-1) {
+		return fmt.Sprintf(formatString, UnknownErrorMessage)
+	}
+
+	return fmt.Sprintf(formatString, FriendlyMessages[e.status])
 }
 
 func (e apnsError) String() string {
